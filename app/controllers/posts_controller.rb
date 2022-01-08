9297bcc44f59388587d3550638ffa9445b50b69c
s-takenoshita <s.takenoshita@gmail.com>
2022-01-08 09:16:04 +0900

    delete -->  not done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update]
+
 
   def index
     @posts = Post.all
     render 'posts/index'
+  end
+
+  def show
+    binding.pry
+
   end
 
   def new
@@ -21,9 +28,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    binding.pry
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to root_path, notice: '削除に成功しました'
+  end
+
   private
     def post_params
       params.require(:post).permit(:content, images: [])
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 
 end

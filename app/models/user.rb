b29590e989296afa20b6090476f31fdcc7725978
s-takenoshita <s.takenoshita@gmail.com>
2022-01-09 14:23:25 +0900

    create rerationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,27 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one_attached :avatar
 
   def has_liked?(post)
     likes.exists?(post_id: post.id)
+  end
+
+  def follow!(user)
+    # user_id = get_user_id(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    # user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 
   def avatar_image

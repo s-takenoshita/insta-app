57e83e41d740c7e743d7a39892766cbb3ac129e2
s-takenoshita <s.takenoshita@gmail.com>
2022-01-10 14:50:37 +0900

    csrfToken

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,8 @@ require("channels")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 document.addEventListener('DOMContentLoaded', () => {
   $('.post_list').on('click', () => {

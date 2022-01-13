127fad261857f5925738db46719566aedaa57be6
s-takenoshita <s.takenoshita@gmail.com>
2022-01-13 21:09:11 +0900

    post.jsをpostIndex.jsとpostShow.jsに分割。

--- app/javascript/packs/postIndex.js
+++ app/javascript/packs/postIndex.js
@@ -3,38 +3,6 @@ import axios from 'axios'
 import { csrfToken } from 'rails-ujs'
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
-const dataset = $('#post_show').data()
-const postId = dataset.postid
-
-const appendNewComment = (comment) => {
-  $('.comments_container').append(
-    `<div class="post_comment"><p>${comment.comment}</p></div>`
-  )
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  // debugger
-  axios.get(`/posts/${postId}/comments`)
-  .then((response) => {
-    const comments = response.data
-    comments.forEach((comment) => {
-      appendNewComment(comment)
-    })
-  })
-})
-
-$('.Comments_btn').on('click', (e) => {
-  const comment = $('#comment').val()
-  axios.post(`/posts/${postId}/comments`, {
-    comment: {comment: comment}
-  })
-    .then((response) => {
-      const comment = response.data
-      appendNewComment(comment)
-      $('#comment').val('')
-    })
-})
-
 $('.inactive-heart').on('click', (e) => {
   e.preventDefault();
   const postId = $(e.currentTarget).attr('id') 

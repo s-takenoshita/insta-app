22de8b54ddf0c337bfdfc9e04c4463c00455e2d2
s-takenoshita <s.takenoshita@gmail.com>
2022-01-18 21:04:26 +0900

    エラーの場合のテスト。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
-  context '内容（content）が入力されている場合' do
+  let!(:user) do
+    user = User.create!({
+      email: 'test@exsample.com',
+      password: '99999999'
+    })
+  end
 
+  context '内容（content）が入力されている場合' do
     # # before方式
     # before do
     #   user = User.create!({
@@ -17,12 +23,6 @@ RSpec.describe Post, type: :model do
     # end
 
     # let方式
-    let!(:user) do
-      user = User.create!({
-        email: 'test@exsample.com',
-        password: '99999999'
-      })
-    end
     let!(:post) do
       post = user.posts.build({
         content: Faker::Lorem.characters(number: 15)
@@ -34,4 +34,16 @@ RSpec.describe Post, type: :model do
     end
   end
 
+  context '内容（content）の文字数が少ない場合' do
+    let!(:post) do
+      post = user.posts.create({
+        content: Faker::Lorem.characters(number: 2)
+      })
+    end
+
+    it 'Postデータを保存できない。' do
+      puts '----> ' + post.errors.messages[:content][0]
+      expect(post.errors.messages[:content][0]).to eq('is too short (minimum is 4 characters)')
+    end
+  end
 end

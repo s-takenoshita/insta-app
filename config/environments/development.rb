cda16e8ba1d78705049f60c35ab8a3684a1614f8
s-takenoshita <s.takenoshita@gmail.com>
2022-01-23 11:16:41 +0900

    letter_openerをgemに定義。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,10 @@ Rails.application.configure do
   # devise 2022/01/03
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # mailer 2022/01/23
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
